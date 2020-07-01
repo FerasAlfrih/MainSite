b791c6f31464f618bf6d662ba957d89e97b9fcd9
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-07-01 23:51:30 +0300

    Update settings.py

--- saferasoft/settings.py
+++ saferasoft/settings.py
@@ -128,6 +128,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
 STATIC_URL = "/static/"
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
