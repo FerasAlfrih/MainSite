5b21d5542d77f852fd75c6ffb0befdfa55fbf005
Feras <alfrih.feras@outlook.com>
2020-07-04 21:58:55 +0300

    yh

--- saferasoft/settings.py
+++ saferasoft/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/MainSite/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_URL = '/static/'
 
